ca305eb743d60fbfa5fc53d11c73b95e1f182daa
dowonl2e <dierwdod@gmail.com>
2022-11-01 18:49:33 +0900

    임시 회원등록

--- src/main/java/com/jewelry/user/UserController.java
+++ src/main/java/com/jewelry/user/UserController.java
@@ -11,24 +11,24 @@ import com.jewelry.user.service.UserService;
 @Controller
 public class UserController {
 
-//	@Autowired
-//	private UserService userService;
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping("/signin")
 	public String signin() {
 		return "user/signin";
 	}
 	
-//	@GetMapping("/signup")
-//	public String signup() throws Exception {
-//		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//		UserTO to = new UserTO();
-//		to.setUser_id("admin");
-//		to.setUser_pwd(passwordEncoder.encode("!@#qwe"));
-//		to.setUser_name("관리자");
-//		to.setUse_yn("Y");
-//		to.setUser_role("ADMIN");
-//		userService.addUser(to);
-//		return "user/signin";
-//	}
+	@GetMapping("/signup")
+	public String signup() throws Exception {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		UserTO to = new UserTO();
+		to.setUser_id("admin");
+		to.setUser_pwd(passwordEncoder.encode("!@#qwe"));
+		to.setUser_name("관리자");
+		to.setUse_yn("Y");
+		to.setUser_role("ADMIN");
+		userService.addUser(to);
+		return "user/signin";
+	}
 }
