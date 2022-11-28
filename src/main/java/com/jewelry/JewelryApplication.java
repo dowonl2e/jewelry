1cfaac11d5c722baa6cd89ce128f6b624e67f3f6
dowonl2e <dierwdod@gmail.com>
2022-11-28 19:43:52 +0900

    추가 설정

--- src/main/java/com/jewelry/JewelryApplication.java
+++ src/main/java/com/jewelry/JewelryApplication.java
@@ -2,14 +2,21 @@ package com.jewelry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass=true)
-public class JewelryApplication {
+public class JewelryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JewelryApplication.class, args);
 	}
-
+	
+	// 배포 시 동작하지 않는 경우 해당 주석을 풀고 다시 빌드
+     @Override
+     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+         return builder.sources(JewelryApplication.class);
+     }
 }
