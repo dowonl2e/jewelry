1476b397d7708bd4809e8b106ee273c0986ec9e6
dowonl2e <dierwdod@gmail.com>
2023-01-09 18:23:50 +0900

    불필요 EnableAspectJAutoProxy 주석

--- src/main/java/com/jewelry/JewelryApplication.java
+++ src/main/java/com/jewelry/JewelryApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy(proxyTargetClass=true)
+//@EnableAspectJAutoProxy(proxyTargetClass=true) spring-boot-starter-aop 의존성이 있을 경우 SpringBoot에서는 기본은 true를 설정하고 있다
 public class JewelryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
